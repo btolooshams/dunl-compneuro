000868dcd90d859d2ced4b92f56c29d9188692d0
Bahareh <btolooshams@gmail.com>
2025-04-18 11:49:26 -0700

    updating setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='dunl',
     version='0.1.0',
+    packages=["src"],
     packages=find_packages(),
     install_requires=[
         'configmypy==0.1.0',
